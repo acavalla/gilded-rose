76eab32341d770c0c60f3bf6f40c02450df4c13c
Anna Cavalla <anna.cavalla@gmail.com>
2021-03-08 11:52:52 +0000

    Bank class responds to deposit with 1 argument

--- bank-tech-test/spec/bank_spec.rb
+++ bank-tech-test/spec/bank_spec.rb
@@ -2,4 +2,10 @@ require 'bank'
 
 describe Bank do
   it { is_expected.to respond_to(:deposit).with(1).argument }
+
+  describe '#deposit' do
+    it 'saves a deposit'
+      subject.deposit(100)
+      expect(subject.deposits[0]).to eq 100    
+  end
 end
