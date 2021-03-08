0366cbb88f90ce99298f9922252676f5eaeea6fe
Anna Cavalla <anna.cavalla@gmail.com>
2021-03-08 12:18:58 +0000

    Started Deposit class to hold deposit info

--- bank-tech-test/lib/bank.rb
+++ bank-tech-test/lib/bank.rb
@@ -4,7 +4,7 @@ class Bank
     @deposits = []
   end
 
-  def deposit(amount)
-    @deposits << amount
+  def deposit(amount, date)
+    @deposits << Deposit.new
   end
 end
--- bank-tech-test/spec/bank_spec.rb
+++ bank-tech-test/spec/bank_spec.rb
@@ -1,12 +1,12 @@
 require 'bank'
 
 describe Bank do
-  it { is_expected.to respond_to(:deposit).with(1).argument }
+  it { is_expected.to respond_to(:deposit).with(2).arguments }
 
   describe '#deposit' do
-    it 'saves a deposit' do
-      subject.deposit(100)
-      expect(subject.deposits[0]).to eq 100
+    it 'initializes a deposit class and pushes it into an array' do
+      subject.deposit(100, "13.01.2020")
+      expect(subject.deposits.length).to eq 1
     end
   end
 end
