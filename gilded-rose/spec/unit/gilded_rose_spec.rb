5da175dc7e73488ce3978a263702f8b9a2790a8f
Anna Cavalla <anna.cavalla@gmail.com>
2021-03-11 17:44:21 +0000

    updated gilded_rose_spec to only test sulfuras

--- gilded-rose/spec/unit/gilded_rose_spec.rb
+++ gilded-rose/spec/unit/gilded_rose_spec.rb
@@ -4,12 +4,8 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  items = [Item.new('foo', 1, 3),
-           Item.new('Sulfuras, Hand of Ragnaros', 2, 80),
-           Item.new('Aged Brie', 2, 49),
-           Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 0),
-           Item.new('Conjured Hand', 4, 6)]
-  gilded_rose = GildedRose.new(items)
+  item = Item.new('Sulfuras, Hand of Ragnaros', 2, 80)
+  gilded_rose = GildedRose.new([item])
 
   describe '#update_quality' do
     before :all do
@@ -18,7 +14,7 @@ describe GildedRose do
 
     context 'Sulfura' do
       it 'keeps quality of sulfura at 80' do
-        expect(items[1].quality).to eq 80
+        expect(item.quality).to eq 80
       end
     end
   end
