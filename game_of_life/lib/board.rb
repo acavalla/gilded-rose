f9252a3f6dddb9448393b7ae56fe8d335a941d25
Anna Cavalla <anna.cavalla@gmail.com>
2021-04-22 13:45:05 +0100

    Simplified board layout

--- game_of_life/lib/board.rb
+++ game_of_life/lib/board.rb
@@ -1,11 +1,6 @@
 class Board
-  attr_reader :columns, :rows
+  attr_reader :layout
   def initialize(columns = 2, rows = 2)
-    @columns = columns
-    @rows = rows
-  end
-
-  def layout
-    [Array.new(columns, 0)] * rows
+    @layout = [Array.new(columns, 0)] * rows
   end
 end
