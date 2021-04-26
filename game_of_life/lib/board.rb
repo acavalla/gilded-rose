19ba41428ae209bad27ce23b50e9936d43f678d5
Anna Cavalla <anna.cavalla@gmail.com>
2021-04-26 20:16:16 +0100

    Increased coverage to 100% and renamed a method to make code clearer

--- game_of_life/lib/board.rb
+++ game_of_life/lib/board.rb
@@ -25,11 +25,10 @@ class Board
 
   def tick
     neighbours
-    new_layout
+    update_live
   end
 
   def neighbours
-    @neighb_tally = new_array(dims)
     @neighb_tally_locs = []
     live.each do |loc|
       label_neighbours(loc)
@@ -41,21 +40,17 @@ class Board
   def label_neighbours(loc)
     NEIGHB_LOC.each do |nloc|
       current_spot = [loc[0] + nloc[0], loc[1] + nloc[1]]
-      if @neighb_tally_locs.any? { |h| h[:location] == current_spot }
-        @neighb_tally_locs.select { |key, _value| key[:location] == current_spot }[0][:tally] += 1
+      if neighb_tally_locs.any? { |h| h[:location] == current_spot }
+        neighb_tally_locs.select { |key, _value| key[:location] == current_spot }[0][:tally] += 1
       else
-        @neighb_tally_locs << { location: current_spot,
+        neighb_tally_locs << { location: current_spot,
                                 tally: 1,
-                                status: live.include?([loc[0] + nloc[0], loc[1] + nloc[1]]) ? 1 : 0 }
+                                status: live.include?(current_spot) ? 1 : 0 }
       end
     end
   end
 
-  def new_array(dims)
-    Array.new(dims) { Array.new(dims, 0) }
-  end
-
-  def new_layout
+  def update_live
     @live = []
     @neighb_tally_locs.select { |key, _value| key[:tally] == 3 }.each { |key| live << key[:location] }
     @neighb_tally_locs.select { |key, _value| key[:tally] == 2 && key[:status] == 1 }.each { |key| live << key[:location] }
